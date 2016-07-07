83048eb328bd5692f51cd6253d007d3040569108
Andrew Roetker <andrew.roetker@puppetlabs.com>
2016-07-07 11:10:05 -0700

    (maint) Configure lein-deploy to know about Puppet Jenkins

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
-(defproject stockpile "0.0.2-SNAPSHOT"
+(defproject stockpile "0.0.1-SNAPSHOT"
   :description "Simple, durable Clojure queuing library"
   ;; :url "http://example.com/FIXME"
   :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[org.apache.commons/commons-lang3 "3.4"]]}})
+  :profiles {:dev {:dependencies [[org.apache.commons/commons-lang3 "3.4"]]}}
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]])
